78d7e3a89954e11c9e13a73f43fbd327f1e3c4b7
jusuf <jusuf_b@hotmail.com>
2017-07-10 13:42:15 +0200

    MainActivity WIP

---
+++ app/src/main/java/se/netmine/hobby_marknad/IMainActivity.java
@@ -0,0 +1,9 @@
+package se.netmine.hobby_marknad;
+
+/**
+ * Created by jusuf on 2017-07-10.
+ */
+
+public interface IMainActivity {
+
+}
--- app/src/main/java/se/netmine/hobby_marknad/MainActivity.java
+++ app/src/main/java/se/netmine/hobby_marknad/MainActivity.java
@@ -1,23 +1,46 @@
 package se.netmine.hobby_marknad;
 
 
-import android.app.ActionBar;
-import android.graphics.drawable.ColorDrawable;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Map;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, IMainActivity {
+
+    private final Context context = this;
+    private Stack<Fragment> fragmentStack = new Stack<Fragment>();
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +49,10 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -76,5 +90,45 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void navigateToFragment(Fragment fragment)
+    {
+//        if( fragmentStack.isEmpty() == false &&
+//                fragment.getClass().getSimpleName().compareTo(fragmentStack.peek().getClass().getSimpleName()) == 0 &&
+//                fragment.getClass().getSimpleName().compareTo(RobotListFragment.class.getSimpleName()) != 0)
+//        {
+//            return;
+//        }
+
+        if( fragmentStack.isEmpty() == false &&
+                fragment.getClass().getSimpleName().compareTo(fragmentStack.peek().getClass().getSimpleName()) == 0 )
+        {
+            return;
+        }
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.setCustomAnimations(R.animator.enter_from_left, R.animator.exit_to_right);
+
+        if(fragmentStack.size() > 0)
+        {
+            fragmentStack.peek().onPause();
+            ft.hide(fragmentStack.peek());
+        }
+        fragmentStack.push(fragment);
+
+        ft.add(R.id.mainContent, fragment);
+        ft.show(fragmentStack.peek());
+        ft.commit();
+
+        //fragmentStack.peek().onResume();
+
+        if(fragmentStack.size() > 1) {
+            toolbar.setNavigationIcon(R.drawable.ic_navigate_back);
+        }
+        else {
+            toolbar.setNavigationIcon(R.drawable.ic_menu_white);
+        }
     }
 }
